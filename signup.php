7d9e3cf930596f0ca5fb2375d14b21f4788b944a
khalidsaif9045 <khalidsaif9045@gmail.com>
2023-09-06 21:50:19 +0530

    Signin Signup backend code

--- signup.php
+++ signup.php
@@ -1,3 +1,29 @@
+<?php  include('dbconn.php');   ?>
+<?php      
+   if( isset($_POST['submitted']) ){
+    $username = $_POST['email'];  
+    $password = $_POST['password'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = md5(mysqli_real_escape_string($con, $password));  
+      
+        $sql = "insert into users (email,password) values('$username','$password')";  
+        $result = mysqli_query($con, $sql);  
+        // $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        // $count = mysqli_num_rows($result);  
+          
+        if($result){  
+            echo "<h1><center> Register successful </center></h1>";  
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid username or password.</h1>";  
+        } 
+   }
+       
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,15 +50,16 @@
       <img src="assets/img/logo.png" id="icon"  width= "100px" height= "200px"/>
     </div>
 
-    <form>
-      <input type="email" id="login" class="fadeIn second" name="login" placeholder="Enter Email" required>
-      <input type="password" id="password" class="fadeIn third" name="login" placeholder="password" required>
-      <input type="submit" class="fadeIn fourth" value="Sign Up" style="margin-top:10px">
+    <form action="" method="POST">
+    <input type='hidden' name='submitted' id='submitted' value='1'/>
+      <input type="email" id="login" class="fadeIn second" name="email" placeholder="Enter Email" required>
+      <input type="password" id="password" class="fadeIn third" name="password" placeholder="password" required>
+      <input type="submit" class="fadeIn fourth" name="Submit" value="Sign Up" style="margin-top:10px">
       
     </form>
 
     <div id="formFooter">
-    <a class="underlineHover" href="signin.php" style="color: #000">No have an account? Sign up</a><br/>
+    <a class="underlineHover" href="signin.php" style="color: #000">Already have an account? Sign In</a><br/>
       <a class="underlineHover" href="#" style="color: #000">Forgot Password?</a>
     </div>
 
