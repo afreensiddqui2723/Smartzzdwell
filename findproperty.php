d73bba299028ba81c33ff1a79a211881a3515775
Khan <afreen.khan@capgemini.com>
2023-10-22 22:36:42 +0530

    Edited version of mobile view dbconn

--- findproperty.php
+++ findproperty.php
@@ -61,7 +61,7 @@ label{
 }
 @media (min-width: 200px) and (max-width: 700px) {
 form .input-group{
-	margin-left: -30px;
+	margin-left: -60px;
 	width: 50vh;
 }
 form label{font-size: 14px !important;}
