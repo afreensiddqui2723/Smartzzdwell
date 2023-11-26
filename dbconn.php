34b7e5d323b3779bf599204ac4346a3216d4d4a6
khalidsaif9045 <khalidsaif9045@gmail.com>
2023-11-26 13:17:15 +0530

    db credentials update

--- dbconn.php
+++ dbconn.php
@@ -1,12 +1,12 @@
 <?php      
-    // $host = "localhost";  
-    // $user = "u869925871_smartzzdwell";  
-    // $password = 'Smartzzdwell2023';  
-    // $db_name = "u869925871_smartzzdwell";  
     $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "smartzzdwell";  
+    $user = "u869925871_smartzzdwell";  
+    $password = 'Smartzzdwell2023';  
+    $db_name = "u869925871_smartzzdwell";  
+    // $host = "localhost";  
+    // $user = "root";  
+    // $password = '';  
+    // $db_name = "smartzzdwell";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
